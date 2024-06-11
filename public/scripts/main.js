685d594534c1354934159be5ae438e467abfd966
Aetinx <sargentcoding@gmail.com>
2024-06-11 14:38:51 +0000

    CSS Variables and other stuff!

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,4 @@
-console.log("hello world")
+document.querySelector(".nav-toggle").onclick = () => {
+  document.querySelector("nav").classList.toggle("open")
+  console.log("Navigation toggled.")
+}
